d2d025f88a016909649b45b3b1dc6408c0613b1d
lollypop27 <lara@adfactory.co.za>
2016-06-14 20:40:32 +0200

    adding code to run successful if there is enough funds to withdraw

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,11 +1,24 @@
 class Atm
   attr_accessor :funds
-  # ATM adding funds
-  def initialize
-    @funds = 1000
-  end
+
+# ATM adding funds
+def initialize
+  @funds = 1000
+end
+
 # ATM Withdrawal
-  def withdraw(amount)
-    @funds -= amount
+def withdraw(amount)
+  @funds -= amount
+end
+
+# Checking that there is enough funds to withdraw
+def withdraw(amount, account)
+  case
+  when amount > account.balance then
+    return
+  else
+  @funds -= amount
+  account.balance = account.balance - amount
+  {status: true, message: 'success', Date.today, amount: amount}
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,13 +1,28 @@
-#Adding funds
 require './lib/atm.rb'
+
+# Adding funds
 describe Atm do
   it 'has 1000$ on intitialize' do
     expect(subject.funds).to eq 1000
-  end
+end
 
 # Withdrawal funds
-  it 'funds are reduced at withdraw' do
+it 'funds are reduced at withdraw' do
     subject.withdraw 50
     expect(subject.funds).to eq 950
-  end
+end
+
+# Successful response
+let(:account) { class_double('Account') }
+
+before do
+  allow(account).to receive(:balance).and_return(100)
+  allow(account).to receive(:balance=)
+end
+
+# Atm needs to know if there is enough funds before transaction is cleared
+it 'allow withdraw if account has enough balance.' do
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expect(subject.withdraw(45, account)).to eq expected_output
+end
 end
