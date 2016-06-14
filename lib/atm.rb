85ebbfc7b48dab74d4fa1e59f10d631fb1dd5230
lollypop27 <lara@adfactory.co.za>
2016-06-14 21:46:10 +0200

    adding withdrawal and success transaction, but still failing

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,24 +1,20 @@
 class Atm
   attr_accessor :funds
 
-# ATM adding funds
-def initialize
-  @funds = 1000
-end
+# adding funds in the atm
+  def initialize
+    @funds = 1000
+  end
 
-# ATM Withdrawal
-def withdraw(amount)
-  @funds -= amount
-end
-
-# Checking that there is enough funds to withdraw
+# Withdrawal
 def withdraw(amount, account)
-  case
-  when amount > account.balance then
+  case # check if there is enough funds
+  when amount > account.balance then # exit method if amount to withdral is biiger than the balance
     return
-  else
-  @funds -= amount
-  account.balance = account.balance - amount
-  {status: true, message: 'success', Date.today, amount: amount}
+  else # If not biger, transaction is performed and deduct the amount
+    @funds -= amount
+    account.balance = account.balance - amount # deducting amount from balance
+    { status: true, message: 'success', date: Date.today, amount: amount } #response for successful Withdraw  al
+  end
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,28 +1,27 @@
 require './lib/atm.rb'
 
-# Adding funds
+# adding double class to manage account and card
 describe Atm do
+  let(:account) { class_double('Account') }
+
+# set value of balance and get a new balance
+    before do
+      allow(account).to receive(:balance).and_return(100)
+      allow(account).to receive(:balance=)
+    end
+# adding funds in the atm
   it 'has 1000$ on intitialize' do
     expect(subject.funds).to eq 1000
-end
-
-# Withdrawal funds
-it 'funds are reduced at withdraw' do
-    subject.withdraw 50
-    expect(subject.funds).to eq 950
-end
-
-# Successful response
-let(:account) { class_double('Account') }
-
-before do
-  allow(account).to receive(:balance).and_return(100)
-  allow(account).to receive(:balance=)
-end
-
-# Atm needs to know if there is enough funds before transaction is cleared
-it 'allow withdraw if account has enough balance.' do
+  end
+# checking the account to see if there is enough money
+  it 'allow withdraw if account has enough balance.' do
     expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
     expect(subject.withdraw(45, account)).to eq expected_output
-end
+  end
+  
+# Withdrawal function
+  it 'funds are reduced at withdraw' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
+  end
 end
