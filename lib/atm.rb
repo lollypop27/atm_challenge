cfc982428152074d97b8709ff6ab9e1ff9f20207
lollypop27 <lara@adfactory.co.za>
2016-06-15 20:32:37 +0200

    adding code if there are no funds in ATM

--- lib/atm.rb
+++ lib/atm.rb
@@ -9,18 +9,17 @@ class ATM
 # Withdrawal of funds
 def withdraw(amount, account)
   case
-  when insufficient_funds_in_account?(account.balance) then
-   { status: false, message: 'insufficient funds', date: Date.today }
-
-  # If not biger, transaction is performed and deduct the amount
-  return
+  when insufficient_funds_in_account(account.balance) then
+    { status: false, message: 'insufficient funds', date: Date.today }
+  when insufficient_funds_in_atm(amount) then
+    { status: false, message: 'insufficient funds in ATM', date: Date.today }
   else
     perform_transaction(amount, account)
   end
 end
 
 private
-
+# insufficient funds in account
 def insufficient_funds_in_account(amount, account)
   amount > account.balance
 end
@@ -30,5 +29,10 @@ def perform_transaction(amount, account)
     @funds -= amount
     account.balance -= account # deducting amount from balance
     { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) } #response for successful Withdrawal
+end
+
+# Not enough funds in the atm
+def insufficient_funds_in_atm?(amount)
+ @funds < amount
 end
 end
