95eb9ba3509cfc8a2658da354dc7352b49d5cf6c
lollypop27 <lara@adfactory.co.za>
2016-06-16 13:13:15 +0200

    adding balance reduced after withdrawal

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -3,6 +3,8 @@ require './lib/atm.rb'
 
   # ATM funds added
   describe Atm do
+    let(:account) { class_double("Account")} #used for both the bank account and card class
+
     it 'has 1000$ on intitialize' do
       expect(subject.funds).to eq 1000
     end
@@ -11,5 +13,11 @@ require './lib/atm.rb'
     it 'funds are reduced at withdraw' do
     subject.withdraw 50
     expect(subject.funds).to eq 950
+  end
+
+  # balnce reduced after withdraw
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
   end
 end
