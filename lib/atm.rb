a35667c16e610afba3cc8a9ccf244dde5bd34c75
lollypop27 <lara@adfactory.co.za>
2016-06-16 13:01:19 +0200

    adding withdrawla of funds

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,15 @@
 class Atm
+
   # ATM funds added
   attr_accessor :funds
+
+  # Balance in account
   def initialize
     @funds = 1000
+  end
+
+  # funds deduct on withdraw
+  def withdraw(amount)
+    @funds -= amount
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,9 +1,15 @@
 
 require './lib/atm.rb'
 
-# ATM funds added
-describe Atm do
-  it 'has 1000$ on intitialize' do
-    expect(subject.funds).to eq 1000
+  # ATM funds added
+  describe Atm do
+    it 'has 1000$ on intitialize' do
+      expect(subject.funds).to eq 1000
+    end
+
+    #funds reduced at withdraw
+    it 'funds are reduced at withdraw' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
   end
 end
