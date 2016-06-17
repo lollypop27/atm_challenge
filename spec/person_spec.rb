54ee336e1da67ba2c3ad8e9800e3147ed7e79912
lollypop27 <lara@adfactory.co.za>
2016-06-17 23:58:41 +0200

    adding persons file along with name and no name in file

---
+++ spec/person_spec.rb
@@ -0,0 +1,14 @@
+require './lib/person'
+require './lib/atm'
+
+describe Person do
+  subject { described_class.new(name: 'Lara')}
+
+  it 'is expected to have a :name on initialize' do
+    expect(subject.name).not_to_be nil
+  end
+
+  it 'is expected to raise error if no name is set' do
+    expect {described_class.new }.to raise_error 'A name is required'
+  end
+end
