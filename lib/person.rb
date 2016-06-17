b955598ffcbf4c8aeab4938cb2f7fe6091657412
lollypop27 <lara@adfactory.co.za>
2016-06-18 00:21:40 +0200

    adding cash to the person

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,18 @@
 class Person
-  attr_accessor :name
+  attr_accessor :name, :cash, :account
 
   def initialize(attrs = {})
     @name = set_name(attrs[:name])
+    @cash = 0
     @account = nil
   end
 
   def create_account
     @account = Account.new(owner: self)
+  end
+
+  def deposit(amount)
+    @account == nil ? missing_account : deposit_funds(amount)
   end
 
   private
@@ -18,6 +23,24 @@ class Person
 
   def missing_name
     rais ArgumentError, 'A name is required'
+  end
+
+  def deposit_funds(amount)
+     @cash -= amount
+     @account.balance += amount
+   end
+
+   def withdraw_funds(args)
+    args[:atm] == nil ? missing_atm : atm = args[:atm]
+    account = @account
+    amount = args[:amount]
+    pin = args[:pin]
+    response = atm.withdraw(amount, pin, account)
+    response[:status] == true ? increase_cash(response) : response
+  end
+
+  def increase_cash(response)
+    @cash += response[:amount]
   end
 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -11,4 +11,8 @@ describe Person do
   it 'is expected to raise error if no name is set' do
     expect {described_class.new }.to raise_error 'A name is required'
   end
+
+  it 'is expected to have :cash attribute with value of 0 on initialize' do
+    expect(subject.cash).to eq 0
+  end
 end
